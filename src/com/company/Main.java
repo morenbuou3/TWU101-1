2d6d805961ab0af42db8402fca6e8babab52b636
king <369558172@qq.com>
2018-07-10 15:45:15 +0800

    feat: printLine

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         PrintAsterisk printAsterisk = new PrintAsterisk();
+
         printAsterisk.print();
+
+        printAsterisk.printLine(8);
     }
 }
--- src/com/company/PrintAsterisk.java
+++ src/com/company/PrintAsterisk.java
@@ -1,7 +1,14 @@
 package com.company;
 
 public class PrintAsterisk {
-    public void print () {
+    public void print() {
         System.out.println("*");
+    }
+
+    public void printLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
